bfa8ce4224777862e64a4291bba32e5a8dcdbad5
drexdev <andrelps.amorim17@gmail.com>
2023-04-23 17:58:38 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client';
 import { Nav } from '@/components/Layout';
+import { useState } from 'react';
 
 export default function Home() {
+	const [ load, setLoad ] = useState('false');
+  
 	return (
-		<Nav />
+		<>
+			<Nav />
+			<h3>{load}</h3>
+		</>
 	);
 }
